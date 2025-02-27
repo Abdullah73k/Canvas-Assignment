73b910b27c1d11d1d773f99525c9a3120b0d4891
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-02-27 17:29:45 -0500

    added a form

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,33 @@
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const shape1 = shape.value;
+	const length1 = length.value;
+	const width1 = width.value;
+    const color1 = color.value;
+	console.log(shape1, length1, width1);
+
+	localStorage.setItem("shape", shape1);
+	localStorage.setItem("length", length1);
+	localStorage.setItem("width", width1);
+    localStorage.setItem("color", color1);
+});
+
+const savedShape = localStorage.getItem("shape");
+const savedLength = localStorage.getItem("length");
+const savedWidth = localStorage.getItem("width");
+const savedColor = localStorage.getItem("color");
+
+
 /** @type {HTMLCanvasElement} */
 let c = document.getElementById("canvas");
-let ctx = c.getContext("2d")
+let ctx = c.getContext("2d");
 
-class Rectangle{
-    constructor(positionX, positionY, length, width, color) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.length = length;
-        this.width = width;
-        this.color = color;
-    }
+class Rectangle {
+	constructor(positionX, positionY, length, width, color) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.length = length;
+		this.width = width;
+		this.color = color;
+	}
 }
-
