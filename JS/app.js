bbd2e67f3c3c478bb11bc887165a4d3afacfc23a
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-02-27 16:55:25 -0500

    rectangle shape added

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
-console.log("test")
+/** @type {HTMLCanvasElement} */
+let c = document.getElementById("canvas");
+let ctx = c.getContext("2d")
+
+class Rectangle{
+    constructor(positionX, positionY, length, width, color) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+}
+
