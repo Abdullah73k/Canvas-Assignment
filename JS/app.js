3fcbba0c4a63c1295790c622b8d8bbc4b79763d3
Abdullah Khamis <abdullahbkhamis@gmail.com>
2025-03-06 09:54:22 -0500

    added 3rd shape, re-adjusted the canvas so drawings are not stretched

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,6 @@
 let rectangleForm = document.getElementById("rectangle");
 let circleForm = document.getElementById("circle");
+let triangleForm = document.getElementById("triangle");
 
 /** @type {HTMLCanvasElement} */
 let c = document.getElementById("canvas");
@@ -13,7 +14,6 @@ class Rectangle {
 		this.height = parseInt(height);
 		this.color = color;
 	}
-
 	draw(ctx) {
 		ctx.fillStyle = this.color;
 		ctx.fillRect(this.positionX, this.positionY, this.width, this.height);
@@ -29,14 +29,7 @@ rectangleForm.addEventListener("submit", (event) => {
 	const positionXRec = document.getElementById("positionX").value;
 	const positionYRec = document.getElementById("positionY").value;
 
-	console.log(
-		"Rectangle:",
-		lengthRec,
-		widthRec,
-		positionXRec,
-		positionYRec,
-		colorRec
-	);
+	console.log("Rectangle:", lengthRec, widthRec, positionXRec, positionYRec, colorRec);
 
 	let shapes = JSON.parse(localStorage.getItem("shapes")) || [];
 
@@ -53,13 +46,7 @@ rectangleForm.addEventListener("submit", (event) => {
 	localStorage.setItem("shapes", JSON.stringify(shapes));
 
 	let lastShape = shapes[shapes.length - 1];
-	let rectangle1 = new Rectangle(
-		lastShape.x,
-		lastShape.y,
-		lastShape.width,
-		lastShape.height,
-		lastShape.color
-	);
+	let rectangle1 = new Rectangle(lastShape.x, lastShape.y, lastShape.width, lastShape.height, lastShape.color);
 
 	rectangle1.draw(ctx);
 });
@@ -71,7 +58,6 @@ class Circle {
 		this.radius = parseInt(radius);
 		this.color = color;
 	}
-
 	draw(ctx) {
 		ctx.beginPath();
 		ctx.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2);
@@ -105,12 +91,63 @@ circleForm.addEventListener("submit", (event) => {
 	localStorage.setItem("shapes", JSON.stringify(shapes));
 
 	let lastShape = shapes[shapes.length - 1];
-	let circle1 = new Circle(
-		lastShape.x,
-		lastShape.y,
-		lastShape.radius,
-		lastShape.color
-	);
+	let circle1 = new Circle(lastShape.x, lastShape.y, lastShape.radius, lastShape.color);
 
 	circle1.draw(ctx);
+});
+
+class Triangle {
+	constructor(x1, y1, x2, y2, x3, y3, color) {
+		this.x1 = parseInt(x1);
+		this.y1 = parseInt(y1);
+		this.x2 = parseInt(x2);
+		this.y2 = parseInt(y2);
+		this.x3 = parseInt(x3);
+		this.y3 = parseInt(y3);
+		this.color = color;
+	}
+	draw(ctx) {
+		ctx.beginPath();
+		ctx.moveTo(this.x1, this.y1);
+		ctx.lineTo(this.x2, this.y2);
+		ctx.lineTo(this.x3, this.y3);
+		ctx.closePath();
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+}
+
+triangleForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const x1 = document.getElementById("x1").value;
+	const y1 = document.getElementById("y1").value;
+	const x2 = document.getElementById("x2").value;
+	const y2 = document.getElementById("y2").value;
+	const x3 = document.getElementById("x3").value;
+	const y3 = document.getElementById("y3").value;
+	const colorT = document.getElementById("colorT").value;
+
+	console.log("Triangle:", x1, y1, x2, y2, x3, y3, colorT);
+
+	let shapes = JSON.parse(localStorage.getItem("shapes")) || [];
+
+	const newTriangle = {
+		type: "triangle",
+		x1: x1,
+		y1: y1,
+		x2: x2,
+		y2: y2,
+		x3: x3,
+		y3: y3,
+		color: colorT,
+	};
+
+	shapes.push(newTriangle);
+	localStorage.setItem("shapes", JSON.stringify(shapes));
+
+	let lastShape = shapes[shapes.length - 1];
+	let triangle1 = new Triangle(lastShape.x1, lastShape.y1, lastShape.x2, lastShape.y2, lastShape.x3, lastShape.y3, lastShape.color);
+
+	triangle1.draw(ctx);
 });
